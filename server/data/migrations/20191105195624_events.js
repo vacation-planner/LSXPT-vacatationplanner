e6e556fe54b4008406ff6c78f75b6f1eada826bd
Abigayle Franklin <abifanklin@gmail.com>
2019-11-05 20:48:54 -0600

    Events seed data added. Start date, start time, end date, end time, and description added to events table.

--- server/data/migrations/20191105195624_events.js
+++ server/data/migrations/20191105195624_events.js
@@ -3,15 +3,14 @@ exports.up = function(knex) {
     return knex.schema.createTable('events', (event) => {
             event.increments();
             event.string('eventName', 128).notNullable();
-            event
-            .string('vacationId')
-            .references('vacation.id');
-            event
-            .string('userUid')
-            .references('users.uid');
-            event
-            .string('secondaryUserId')
-            .references('secondaryUsers.id');
+            event.string('vacationId').references('vacation.id');
+            event.string('userUid').references('users.uid');
+            event.string('secondaryUserId').references('secondaryUsers.id');
+            event.date('startDate');
+            event.date('endDate');
+            event.time('startTime');
+            event.time('endTime');
+            event.string('description', 140);
         })
 };
 
