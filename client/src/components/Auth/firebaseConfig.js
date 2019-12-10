520cae3f90fcac18c8d11da82c31685e6af32ffb
willieino <willieino@hotmail.com>
2019-12-09 21:50:44 -0800

    modify firebase settings

--- client/src/components/Auth/firebaseConfig.js
+++ client/src/components/Auth/firebaseConfig.js
@@ -7,9 +7,21 @@ const config = {
   databaseURL: process.env.REACT_APP_DATABASE,
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE,
-  messagingSenderId: process.env.REACT_APP_MESSAGE
+  messagingSenderId: process.env.REACT_APP_MESSAGE,
+  appId: process.env.REACT_APP_ID,
+  measurementId: process.env.REACT_MEASUREMENT_ID
 };
+
+/* const config = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE,
+  messagingSenderId: process.env.REACT_APP_MESSAGE
+}; */
 
 const fireApp = firebase.initializeApp(config);
 
 export const fire = fireApp.auth();
+
