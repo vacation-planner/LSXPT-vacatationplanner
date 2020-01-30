3aff884394086c5dff8eebab34ec5c3c6388c476
alasalle <angela.lasalle91@gmail.com>
2020-01-29 22:09:19 -0500

    new branch to start from

--- server/constants/stripe.js
+++ server/constants/stripe.js
@@ -1,6 +1,7 @@
 const configureStripe = require('stripe');
 
 const STRIPE_SECRET_KEY = process.env.NODE_ENV === 'production'
+// probably should put these keys in env
     ? 'sk_test_QaMreRWGOmBAJroixHITF1fV00VXRQSxsN'
     : 'sk_test_QaMreRWGOmBAJroixHITF1fV00VXRQSxsN';
 
