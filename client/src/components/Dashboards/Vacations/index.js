75ce5a810e28665b5f1c37d29849a307a7a41df9
Axis <atonymartin20@yahoo.com>
2020-02-24 19:57:23 -0600

    Vacation Details page styled and deleteVacationModal added

--- client/src/components/Dashboards/Vacations/index.js
+++ client/src/components/Dashboards/Vacations/index.js
@@ -19,30 +19,28 @@ import { AppContext } from '../../Context/AppContext.js';
 
 // const URL = "http://localhost:5500/api";
 //const URL = 'https://vacationplannerlx.herokuapp.com/api';
-  
+
 class Vacations extends Component {
   constructor(props) {
     super(props);
-  this.state = {
-    uid: "",
-    value: "",
-    title: this.props.title,
-    vacationsId: this.props.vacationsId,
-    premium: this.props.premium,
-   };
-};
+    this.state = {
+      uid: "",
+      value: "",
+      title: this.props.title,
+      vacationsId: this.props.vacationsId,
+      premium: this.props.premium,
+    };
+  };
 
 
-render() {
-   
+  render() {
+
     return (
       <div className="vacations">
         <GridContainer>
-            <GridItem xs={12} sm={12} md={4}>
-            {/*  <AddUsers>  </AddUsers>  */}
-                          <Display title={this.state.title} vacationsId={this.state.vacationsId} >
-                        </Display> 
-            </GridItem>
+          <GridItem xs={12} sm={12} md={4}>
+            <Display title={this.state.title} vacationsId={this.state.vacationsId} />
+          </GridItem>
         </GridContainer>
       </div>
     );
