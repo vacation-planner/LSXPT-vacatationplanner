a86c0581a2d9b2bf82faf735c48e6d8ab6c64307
willieino <willieino@hotmail.com>
2020-03-17 22:47:06 -0700

    modify expenses component

--- client/src/components/Dashboards/Expenses/index.js
+++ client/src/components/Dashboards/Expenses/index.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import axios from "axios";
 import { fire } from "../../Auth/firebaseConfig";
 // Components
-//import AddExpenses from "./addExpenses.js"
 import GridContainer from "../../StyledComponents/Dashboards/Expenses/js/GridContainer.js";
 import GridItem from "../../StyledComponents/Dashboards/Expenses/js/GridItem.js";
 import Card from "../../StyledComponents/Dashboards/Expenses/js/Card.js";
@@ -30,19 +29,17 @@ class Expenses extends Component {
     eventName: "",
     vacationsTitle: this.props.title,
     vacationsId: this.props.vacationsId,
-    //startDateTime: "",
-    //endDateTime: "",
     description: "",
     eventsId: "",
     events: [],
-    disabled: false,
+    disabled: true,
     deleteDisabled: true,
     participant: "",
     secondaryUsersId: 1,
     secondaryUsers: [],
-    secondaryUsersExpense: 0,
+    secondaryUsersExpense: "",
     expenses: [],
-    expenseOwed: 0,
+    expenseOwed: "",
     secondaryUsersFirstName: "",
     secondaryUsersLastName: "",
     checked: false,
@@ -65,26 +62,25 @@ class Expenses extends Component {
       }); 
     };
 
+    // function saves the new expense to the database
   saveExpense = () => {
     let eventName = "";
     if (this.state.eventsId !== "") {
       eventName = this.state.eventName
     }
-    let expenseRec = {
-      //title: this.state.title,
-      eventsId: this.state.eventsId,
-      vacationsId: this.props.vacationsId,
-      eventName: eventName,
-      vacationsTitle: this.props.title,
-      secondaryUsersId: this.state.secondaryUsersId,
-      expenseOwed: this.state.expenseOwed,
-      secondaryUsersExpense: this.state.secondaryUsersExpense,
-      secondaryUsersFirstName: this.state.participant,
-    }
+    if (this.state.secondaryUsersExpense !== "") {
+      if (this.state.expenseOwed !== "") {
+        let expenseRec = {
+          eventsId: this.state.eventsId,
+          vacationsId: this.props.vacationsId,
+          eventName: eventName,
+          vacationsTitle: this.props.title,
+          secondaryUsersId: this.state.secondaryUsersId,
+          expenseOwed: this.state.expenseOwed,
+          secondaryUsersExpense: this.state.secondaryUsersExpense,
+          secondaryUsersFirstName: this.state.participant,
+        }
   
- /*    axios
-    .post(`${URL}/expenses/`, expenseRec)
-    .then(response => { */
   axios
   .post('/expenses/', expenseRec)
   .then(response => {
@@ -95,28 +91,14 @@ class Expenses extends Component {
     })
     .catch(err => {
       console.log('We"ve encountered an error');
-    });  
+    }); 
+  } 
+} else {
+  alert("Expense fields cannot be empty")
 }
+  }
 
- /*  fetchId = eventName => {
-    axios
-      .get('/events')
-      .then(response => {
-        response.data.forEach((item, index) => {
-          if (item.eventName === this.state.eventName) {          
-            this.setState({
-                eventsId: item.id,
-                disabled: false
-            });
-          }
-        });
-      })
-      .catch(err => {
-        console.log('We"ve encountered an error');
-      });
-  }; */
-
-  // this function grabs the list of secondary users from the table
+   // this function grabs the list of secondary users from the table
   fetchSecondaryUsers = (vacationsId) => {
     let secondaryUsers = [];
     axios
@@ -157,67 +139,39 @@ class Expenses extends Component {
     axios
     .get(`/expenses/events/${eventsId}`)
     .then(response => {         
+      //console.log('response data: ', response.data);
       this.fetchExpenseUpdate(response.data, secondaryUsersId);
-      //response.data.forEach((expense, index) => {
-      //  console.log('in expenses: ', expense.eventsId);
-     //   if (expense.eventsId === eventsId) {          
-     //     expenses.push(expense)
-   /*  .get('/events/')
-    .then(response => {
-      response.data.forEach((event, index) => {
-        if (event.vacationsId === vacationsId) {          
-            events.push(event) */
-       // })
-        // check the expenses array for a match to the secondary users id
+   
       })     
-    
-      //console.log('expenses: ', this.state.expenses);  
-    //})
+ 
     .catch(err => {
       console.log('We"ve encountered an error');
     });
-    console.log('prexpenses: ', expenses);
+    //console.log('prexpenses: ', expenses);
     this.setState({
       expenses: expenses
     });    
-   // this.fetchExpenseUpdate(this.state.expenses, secondaryUsersId);
-   //expenses.forEach((expense, index) => {
-    //  if (expense.secondaryUsersId === secondaryUsersId) {          
-    //       this.setState({
-            /* title: expense.title, */
-    //        eventsId: expense.eventsId, 
-            /* vacationsId: expense.vacationsId, */
-            /* eventName: expense.eventName, */
-            /* vacationsTitle: expense.vacationsTitle, */
-            /* secondaryUsersId: this.state.secondaryUsersId, */
-            /* amount: expense.amount, */
-     //       secondaryUsersExpense: expense.secondaryUsersExpense,
-     //       expenseOwed: expense.expenseOwed,
-            /* secondaryUsersName: this.state.participant, */
-    //        });  
-    //        console.log('in expenses');
-        /* expenses.push(expense) */
-    //  }
-      // check the expenses array for a match to the secondary users id
-   // });     
+   
   };
 
   fetchExpenseUpdate = (expenses, secondaryUsersId) => {
-    console.log('in newFetchexpenses');
+    let match = false;
     expenses.forEach((expense, index) => {
-    if (expense.secondaryUsersId === secondaryUsersId) {          
-             
+    
+      if (expense.secondaryUsersId === secondaryUsersId) {          
+      match = true;       
       this.setState({
               secondaryUsersExpense: expense.secondaryUsersExpense,
               expenseOwed: expense.expenseOwed,
               }) 
             }
     else {
+      if (match === false) {
       this.setState({
         secondaryUsersExpense: "",
         expenseOwed: "",
         }) 
-
+      }
     }
          });
     
@@ -225,7 +179,7 @@ class Expenses extends Component {
 
   // this function grabs the event record for a single event
   fetchEvent = (eventsId) => {
-    //let events = [];
+   
     axios
     .get(`/events/${eventsId}`)
     .then(response => {         
@@ -233,9 +187,6 @@ class Expenses extends Component {
                 eventsId: eventsId,
                 eventName: response.data.eventName,
                 cost: response.data.cost,
-                //startDateTime: response.data.startDateTime,
-                //endDateTime: response.data.endDateTime,
-                //description: response.data.description,
             }); 
     })
     .catch(err => {
@@ -262,19 +213,21 @@ class Expenses extends Component {
       console.log('We"ve encountered an error');
     });
   };
+
   // the user has clicked on a line item in the secondaryUser box
   listSelect = (id) => {        
     this.fetchSecondaryUser(id);
     // Also need to grab all the expense data !!!
-    console.log('eventsId: ', this.state.eventsId);
-    console.log('secondaryUsersId: ', this.state.secondaryUsersId);
+    //console.log('eventsId: ', this.state.eventsId);
+    //console.log('secondaryUsersId: ', this.state.secondaryUsersId);
     this.fetchExpense(this.state.eventsId, id);
   };
 
   // the user has clicked on a line item in the events box
   eventSelect = (id) => { 
     this.setState({
-      listVisible: true
+      listVisible: true,
+      disabled: false,
     });       
     this.fetchEvent(id);
   };
@@ -311,128 +264,108 @@ render() {
   return (
     <div className={classes.parent}>
       <Zoom in={checked} > 
+        
         <GridContainer>
           <GridItem xs={12} sm={12} md={12}>
+          <CardBody className={classes.titleBox}>
             <h3>Create Expense: {this.state.eventName}</h3>
             <h4>Current Vacation: {this.props.title}</h4>
+            </CardBody>
             <CardBody   className={classes.cardBody2}>
               <CardBody  className={classes.cardBodyContainer1}>
-               {/*  <CardBody  xs={12} sm={12} md={12}> */}
-                 {/*  <div className="eventContainer"> */}
-                   {/*  <div className="left">   */}                 
-                     {/*  <CardBody  className={classes.cardBodyContainer2}> */}
-                        <CardBody> 
-                          <h4>Available Events:</h4>{" "}                                
-                          <div className="eventsList">
-                              {this.eventList()} 
-                          </div>          
-                        </CardBody>
-                        <CardBody> 
-                          <h4>Vacation Participants:</h4>{" "}  
-                          {this.state.listVisible ? (
-                            <div className="participantsList">
-                              {this.participantList()}                                        
-                            </div>) : null}
-                        </CardBody>
-                    {/*   </CardBody > */}
-                    
-                    
-                   {/*  </div> */}
-                  
-                    <Tooltip
-                      placement="top"
-                      disableFocusListener
-                      title={
-                        <Typography color="inherit" variant="h5">
-                          Create an expense and optionally assign it to an event. To do this, click on the event before saving.
-                        </Typography>
-                      }
-                    > 
-                    <div className="right">
-                      <p> 
-                        Event Name (select from list):
-                      </p> 
-                        <div className="eventNameExp">
-                          <p>{this.state.eventName}</p>
-                        </div>
-                      <p>
-                        Participant (select from list): 
-                      </p>            
-                        <div className="participant">
-                          <p> {this.state.participant}</p>
-                        </div>
-                      <p>
-                        Total Event Cost: 
-                      </p>
-                      <p>
-                        <NumberFormat
-                          value={this.state.cost}
-                          displayType={"text"}
-                          thousandSeparator={true}
-                          prefix={"$"}
-                          className="cost"
-                        />
-                      </p>
-                      <p>Participant Cost: 
-                      {/* <NumberFormat
-                        value={this.state.secondaryUsersExpense}
-                        displayType={"text"}
-                        thousandSeparator={true}
-                        prefix={"$"}
-                        className="secondaryUsersExpense"
-                      /> */}  <input
-                          type="text"
-                          name="secondaryUsersExpense"
-                          onChange={this.handleChange}
-                          value={this.state.secondaryUsersExpense}
-                          className="secondaryUsersExpense"
-                        /> 
-                      </p>
-                      <p>Amount Participant Owes: 
-                      {/* <NumberFormat
-                        value={this.state.expenseOwed}
-                        displayType={"text"}
-                        thousandSeparator={true}
-                        prefix={"$"}
-                        className="expenseOwed"
-                      /> */}
-                      <input
-                          type="text"
-                          name="expenseOwed"
-                          onChange={this.handleChange}
-                          value={this.state.expenseOwed}
-                          className="expenseOwed"
-                        /> 
-                         </p><p> </p>
-                      <CardBody className={classes.btnContainer}>
-                      <Button
-                        color="rose"
-                        onClick={() => this.saveExpense()} 
-                        disabled={this.state.disabled}
-                        className="expButton"
-                      >
-                      Save
-                      </Button>
-                      <Button           
-                        color="rose"
-                        onClick={() => this.deleteExpense()}
-                        disabled={this.state.deleteDisabled} 
-                        className="deleteExpense"
-                      >
-                      Cancel
-                      </Button>
-                      </CardBody>
-                    </div>
-                  </Tooltip>
-                {/* </div> */}
-              </CardBody>
-           {/*  </CardBody> */}
-          {/* </CardBody > */}
-          
+                <CardBody> 
+                  <h4>Available Events:</h4>{" "}                                
+                  <div className="eventsList">
+                    {this.eventList()} 
+                  </div>          
+                </CardBody>
+                <CardBody> 
+                  <h4>Vacation Participants:</h4>{" "}  
+                  {this.state.listVisible ? (
+                  <div className="participantsList">
+                    {this.participantList()}                                        
+                  </div>) : null}
+                </CardBody> 
+                </CardBody> 
+                <CardBody   className={classes.cardBodyContainer2}>           
+              <Tooltip
+                placement="top"
+                disableFocusListener
+                title={
+                  <Typography color="inherit" variant="h5">
+                    Select an event and participant then add the expenses, Save before exiting.
+                  </Typography>
+                }
+              > 
+                <div className="right">
+                  <p> 
+                    Event Name (select from list):
+                  </p> 
+                  <div className="eventNameExp">
+                    <p>{this.state.eventName}</p>
+                  </div>
+                  <p>
+                    Participant (select from list): 
+                  </p>            
+                  <div className="participant">
+                    <p> {this.state.participant}</p>
+                  </div>
+                  <p>
+                    Total Event Cost: 
+                  </p>
+                  <p>
+                    <NumberFormat
+                      value={this.state.cost}
+                      displayType={"text"}
+                      thousandSeparator={true}
+                      prefix={"$"}
+                      className="cost"
+                    />
+                  </p>
+                  <p>Participant Cost: 
+                    <input
+                      type="text"
+                      name="secondaryUsersExpense"
+                      onChange={this.handleChange}
+                      value={this.state.secondaryUsersExpense}
+                      className="secondaryUsersExpense"
+                    /> 
+                  </p>
+                  <p>Amount Participant Owes: 
+                    <input
+                      type="text"
+                      name="expenseOwed"
+                      onChange={this.handleChange}
+                      value={this.state.expenseOwed}
+                      className="expenseOwed"
+                    /> 
+                  </p>
+                  <p> </p>
+                 
+                </div>
+              </Tooltip>
+            </CardBody> 
           </CardBody>   
-          <CardBody  
-            className={classes.cardBodyContainer3}>
-          </CardBody>                     
+          {/* <CardBody className={classes.cardBodyContainer3}> */}
+             <CardBody className={classes.btnContainer}>
+                    <Button
+                      color="rose"
+                      onClick={() => this.saveExpense()} 
+                      disabled={this.state.disabled}
+                      className="expButton"
+                    >
+                      Save
+                    </Button>
+                    <Button           
+                      color="rose"
+                      onClick={() => this.deleteExpense()}
+                      disabled={this.state.deleteDisabled} 
+                      className="deleteExpense"
+                    >
+                      Cancel
+                    </Button>
+                  </CardBody>
+          {/* </CardBody>    */}                  
         </GridItem>
       </GridContainer>
     </Zoom>
--- client/src/components/StyledComponents/Dashboards/Expenses/styles.js
+++ client/src/components/StyledComponents/Dashboards/Expenses/styles.js
@@ -4,25 +4,41 @@ const styles = theme => ({
         display: 'flex',
         flexDirection: 'column',
         alignItems: 'center',
-        borderTop: "12px solid pink",
+        //borderTop: "12px solid purple",
         //width: '90%',
-        maxWidth: '400px',
-        padding: '30px',
+        backgroundImage: 'url("../../../images/42545.jpg")',
+        width: '600px',
+        /* padding: '30px', */
+        marginTop: '20px',
         marginLeft: '30px',
         borderRadius: '15px',
+        borderTop: "12px solid #ea1e63",
         backgroundColor: '#FFFFFF',
         boxShadow: '0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24)',
+        [theme.breakpoints.up("sm")]: {
+            width: '540px',   
+           /*  width: "100%",   */ 
+          },
+        [theme.breakpoints.down('sm')]: {
+            /* border: '0', */
+            width: '80%',
+            borderTop: "8px solid #ea1e63",
+            /* alignItems: 'center', */
+            boxShadow: '0 0 0, 0 0 0'
+        },
         [theme.breakpoints.down('xs')]: {
             border: '0',
+           /*  width: '100%', */
+            boxShadow: '0 0 0, 0 0 0',
+            borderTop: "4px solid #ea1e63",
+            borderRadius: '0px',
+            marginLeft: '0px',
             width: '100%',
-            boxShadow: '0 0 0, 0 0 0'
+            padding: "5px",
+            margin: "auto, auto",
+            //backgroundColor: '#FAFAFA',
         },
-        [theme.breakpoints.down('sm')]: {
-            border: '0',
-            width: '90%',
-            alignItems: 'center',
-            boxShadow: '0 0 0, 0 0 0'
-        },
+        
     },
    /*  cardBody: {
         display: "flex",
@@ -32,57 +48,70 @@ const styles = theme => ({
     }, */
     cardBody2: {
       display: "flex",
-      //backgroundColor: '#E91E63',
-      flexDirection: "Column",
+     /*  backgroundColor: '#E91E63',  */
+      flexDirection: "row",
       alignItems: 'center',
       margin: 'auto',
       [theme.breakpoints.up("sm")]: {
-        width: '250px',   
+        width: '540px',   
        /*  width: "100%",   */ 
       },
       [theme.breakpoints.down("sm")]: { 
         flexDirection: "column",
         alignItems: 'center',
-        width: '225px',  
+        width: '250px',  
     }
   },
   btnContainer: {
     display: "flex",
-    //backgroundColor: '#E91E63',
+    backgroundColor: 'green',
+    justifyContent: 'center',
     flexDirection: "row",
     alignItems: 'center',
-    margin: 'auto',
+    backgroundColor: '#E91E63',
+    /* marginLeft: '100px',  */
+    /* marginRight: '0px', */
     [theme.breakpoints.up("sm")]: {
-       width: '250px',    
-     /*  width: "100%",   */ 
+      /*  width: '250px',  */   
+       width: "100%",  
     },
     [theme.breakpoints.down("sm")]: { 
       flexDirection: "column",
       alignItems: 'center',
-      width: '225px',  
+      /* width: '100%', */  
   }
 },
- /*  cardBodyContainer1: {
+   cardBodyContainer1: {
       display: "flex",
-      backgroundColor: 'green',
+      /* backgroundColor: 'orange', */
       flexDirection: "Column",
       [theme.breakpoints.up("sm")]: {
-          width: "100%",   
+          maxWidth: "260px", 
+          height: '385px',  
       },
       [theme.breakpoints.down("sm")]: { 
-        width: '100%',
-        maxWidth: "250px",  
-    }
-  }, */
- /*  cardBodyContainer2: {
+       /*  width: '100%', */
+        width: "250px",  
+    },
+    [theme.breakpoints.down("xs")]: { 
+     /*  width: '100%', */
+      width: "250px",  
+  }
+  }, 
+   cardBodyContainer2: {
       display: "flex",
       flexDirection: "Column",
-      backgroundColor: 'red',
+     /*  backgroundColor: 'gray', */
       alignItems: 'center',  
       [theme.breakpoints.up("sm")]: {
-          width: "50%",   
-      }
-  }, */
+          maxWidth: "250px", 
+          height: '385px',   
+      },
+      [theme.breakpoints.down("sm")]: { 
+       /*  width: '100%', */
+        width: "250px",  
+    }
+  }, 
   cardBodyContainer3: {
       display: "flex",
       flexDirection: "row",
